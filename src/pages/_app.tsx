99c87e7ed5b7c668e9314e04752d2897b661c625
Brenda Profiro <brendaprofiro@gmail.com>
2022-03-13 17:46:22 -0300

    feat: Create initial structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { globalStyles } from "../styles/global";
+
+function MyApp({ Component, pageProps }) {
+  globalStyles();
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
